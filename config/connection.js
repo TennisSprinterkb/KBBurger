713c760c2350794b1eb941981fa71258aff7a8ed
Prince Avila_b <43385846+TennisSprinterkb@users.noreply.github.com>
2019-02-27 21:10:56 -0500

    PorT update

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 require("dotenv").config();
 var connection;
 
-if (process.env.PORT) {
-    connection = mysql.createConnection(process.env.PORT);
+if (process.env.PORT.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.PORT.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
     host:'localhost',
